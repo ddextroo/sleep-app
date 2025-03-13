8da0ac2c14a10a25ac3c1ec1802b9278a993ebc6
ddextroo <dexteringuito@gmail.com>
2025-03-13 16:30:06 +0800

    qqq

--- src/app/(onboarding)/welcome.tsx
+++ src/app/(onboarding)/welcome.tsx
@@ -2,6 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { router } from "expo-router";
 import AsyncStorage from "@react-native-async-storage/async-storage";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Welcome() {
   const completeOnboarding = async () => {
@@ -9,8 +10,15 @@ export default function Welcome() {
     router.replace("/(auth)/login");
   };
   return (
-    <View>
-      <Text>Welcome</Text>
-    </View>
+    <SafeAreaView className="flex-1">
+      <View className="flex-col">
+        <View className="h-1/2 bg-gray-200 p-4">
+          <Text>Left Half</Text>
+        </View>
+        <View className="h-1/2 bg-gray-400 p-4">
+          <Text>Right Half</Text>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 }
